c780f7cccd806cf4896d7d4b0a19229909acc52c
shimpleRan <aksent1@naver.com>
2022-01-26 21:31:52 +0900

    추상메서드, abstract

---
+++ 20220126/src/abstract1/Child.java
@@ -0,0 +1,13 @@
+package abstract1;
+
+public class Child extends Parent {
+     
+	//getAge를 오버라이딩해주세요.
+	// "부모쪽 나이는 private이라 조회가 안됩니다" 라고 출력해주세요
+
+	@Override
+	public void getAge() {
+		System.out.println("부모쪽 나이는 private이라 조회가 안됩니다");
+		
+	}
+}
